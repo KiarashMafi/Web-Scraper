696c82fa3557d8434908c166cc51fdadf39bfcad
KiarashMafi <kiarash.mafi.2007@gmail.com>
2023-09-16 16:46:44 +0330

    Printing the 5 hours table

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 import requests
 import numpy as np
 from bs4 import BeautifulSoup
@@ -16,15 +15,12 @@ temperature = temp_div.text
 
 upcoming_5hour_table = webpage.find("table", id="wt-5hr")
 next_hours = np.array(upcoming_5hour_table.find("tr", class_=("h2")))
-next_hours_temps = list(upcoming_5hour_table.find("tr", class_="h2 soft").text)
+next_hours_temps = np.array(upcoming_5hour_table.find("tr", class_="h2 soft"))
+print(len(next_hours_temps), len(next_hours))
 
-# next_hours_temps = next_hours_temps.decode("utf-8")
-# next_hours_temps = re.findall(r"\d+", next_hours_temps)
+next_hours_table = {}
+for i in range(6):
+    next_hours_table = {next_hours[i][0]: next_hours_temps[i][0]}
 
-print(next_hours_temps)
-
-
-
-# Result: ['3', '0', '\xa0', '°', 'C', '3', '0', '\xa0', '°', 'C', '2', '8', '\xa0', '°', 'C', '2', '8', '\xa0', '°', 'C', '2', '6', '\xa0', '°', 'C', '2', '6', '\xa0', '°', 'C']
 # Access Token: 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
 
